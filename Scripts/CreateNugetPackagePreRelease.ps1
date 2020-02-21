eb51065fe831163cfc8c086a2fb2d64bac8e9a8d
Mark Dickinson <marksamdickinson@gmail.com>
2020-02-21 07:41:39 -0700

    Update CreateNugetPackagePreRelease.ps1

--- Scripts/CreateNugetPackagePreRelease.ps1
+++ Scripts/CreateNugetPackagePreRelease.ps1
@@ -1,4 +1,5 @@
 $Name = "DickinsonBros.Logger"
 $DateTime = [datetime]::UtcNow.ToString("yyyyMMdd-HHmmss")
 $PackageID = $Name +  "ci-" + $DateTime
-dotnet pack $Name -c Release  -p:PackageID=$PackageID  --output C:\Packages 
+dotnet pack $Name -c Release  -p:PackageID=$PackageID  --output C:\Packages  
+

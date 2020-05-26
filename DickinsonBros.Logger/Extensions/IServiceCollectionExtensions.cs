2d28e5d9abf7629d5658130a53519f8d119cc142
Mark Dickinson <marksamdickinson@gmail.com>
2020-05-26 15:12:05 -0700

    Added Extensions, Updated Runner

---
+++ DickinsonBros.Logger/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using DickinsonBros.Logger.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DickinsonBros.Logger.Extensions
+{
+
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddLoggingService(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped(typeof(ILoggingService<>), typeof(LoggingService<>));
+            serviceCollection.AddSingleton<ICorrelationService, CorrelationService>();
+
+            return serviceCollection;
+        }
+    }
+
+}
